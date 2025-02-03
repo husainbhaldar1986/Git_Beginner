6a631ef7b155b25715c1e27e20cde2f6caf511ad
husainbhaldar <husain.bhaldar@assurant.com>
2025-02-03 14:15:46 +0530

    New Project created

---
+++ src/test/java/Module1_Demo1/Demo1_Test1.java
@@ -0,0 +1,10 @@
+package Module1_Demo1;
+
+public class Demo1_Test1 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Run Demo1 Test1");
+	}
+
+}
---
+++ src/test/java/Module1_Demo1/Demo1_Test2.java
@@ -0,0 +1,9 @@
+package Module1_Demo1;
+
+public class Demo1_Test2 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Run Demo1 Test2");
+	}
+}
